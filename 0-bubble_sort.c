34a74d72b05f7ed9bc5d3685a5715813ad2f612d
Opeyemi Akande <akande212@gmail.com>
2022-07-18 07:18:47 +0100

    The bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * Author: Opeyemi Akande
+ *
+ * bubble_sort - a function to sort an array of integers using the 
+ * bubble sort algorithm
+ *
+ * @array: the array of integers to be sorted
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, sorted;
+	int temp;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		sorted = 0;
+		for (j = 0; j < (size - 1 - i); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+				sorted = 1;
+			}
+		}
+		
+		if (sorted == 0)
+			break;
+	}
+}
