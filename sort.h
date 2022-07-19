870d67d72bb7fb5ef6b900b6ad12ee98caad9377
Opeyemi Akande <akande212@gmail.com>
2022-07-19 05:39:39 +0100

    Corrections

--- sort.h
+++ sort.h
@@ -1,8 +1,23 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef SORT_H
+#define SORT_H
 
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
